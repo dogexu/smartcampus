80c4b12648584764b268971aa9be5715afcb6ad8
caitongbo <461892420@qq.com>
2018-06-28 10:39:11 +0800

    更新

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # smartcampus
 
-开发环境及工具
+1.开发环境及工具
 
-    jdk1.8 + mysql5.7 + tomcat + IDEA
-
-系统架构
-
-    Spring+Springmvc+Maven+Mybatis+BootStrap
+    jdk1.8 + mysql5.7 + tomcat7/8 + IDEA
     
-导入IDEA 
+2.系统架构
+
+    Spring + Springmvc + Maven + Mybatis + BootStrap   
+    
+3.导入IDEA 
     
     方式一：
     
@@ -17,16 +17,22 @@
     
     方式二：
     
-      下载仓库到本地并解压，打开Intellij IDEA-->Open（选择项目解压路径），找到porm.xml文件并打开，待所有架包下载导入完成即可
-      
-  导入数据库
+      下载仓库到本地并解压，打开Intellij IDEA-->Open（选择项目解压路径），找到porm.xml文件并打开，待所有架包下载导入完成即可     
+
+4.导入MySQL数据库
+  
+    .sql文件存放路径： resources/db 
   
      1.直接导入选择smartcampus.sql
-     2.手动执行要以文本方式打开手动建库xx.sql，复制语句执行
-     
-  修改数据库连接配置
-  
-      路径为项目 resources/jdbc.properties
-    
+     2.手动执行打开手动建库smartcampus.txt，复制语句执行     
+
+5.修改JDBC及MyBatis数据库连接账户
+
+    JDBC配置文件存放路径：resources/jdbc.properties
+      
+    MyBatis配置文件存放路径：resources/generatorConfig.properties
+
+6.IDEA部署tomcat执行
+
     
     
--- src/main/resources/db/smartcampus.sql
+++ src/main/resources/db/smartcampus.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50722
  File Encoding         : 65001
 
- Date: 26/06/2018 10:55:39
+ Date: 28/06/2018 10:36:54
 */
 
 SET NAMES utf8mb4;
@@ -26,12 +26,14 @@ CREATE TABLE `authority`  (
   `au_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '权限名，比如老师、学生、商户',
   `au_details` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '权限职能描述',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of authority
 -- ----------------------------
-INSERT INTO `authority` VALUES (1, '1', '1');
+INSERT INTO `authority` VALUES (1, '学生', '我的一卡通，我的订单');
+INSERT INTO `authority` VALUES (2, '教师', '我的一卡通 审核学生申请的一卡通 我的订单');
+INSERT INTO `authority` VALUES (3, '商户', '我的商品 我要上架 我要下架');
 
 -- ----------------------------
 -- Table structure for card
@@ -50,7 +52,7 @@ CREATE TABLE `card`  (
 -- ----------------------------
 -- Records of card
 -- ----------------------------
-INSERT INTO `card` VALUES (1, '1', '1', '1', 1, '1');
+INSERT INTO `card` VALUES (1, '1', '5000000', '2018001', 1, '1');
 
 -- ----------------------------
 -- Table structure for goods
@@ -64,17 +66,21 @@ CREATE TABLE `goods`  (
   `g_vid` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '条形码',
   `g_img` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品图片',
   `g_simg` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品缩略图',
-  `g_label` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ' 商品描述',
+  `g_label` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品描述',
   `g_details` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品详情',
-  `g_state` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '商品状态 1上架 0下架 -1缺货',
+  `g_state` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '商品状态 1上架 0下架 -1缺货',
   `s_i_uuid` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '商家表ID',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of goods
 -- ----------------------------
-INSERT INTO `goods` VALUES (1, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1');
+INSERT INTO `goods` VALUES (1, 'Aptamil爱他美', '195', '天猫', '13146767845', 'https://gw.alicdn.com/bao/uploaded/i1/725677994/TB2VLAgzf1TBuNjy0FjXXajyXXa_!!725677994-0-sm.jpg', 'https://gw.alicdn.com/bao/uploaded/i1/725677994/TB2VLAgzf1TBuNjy0FjXXajyXXa_!!725677994-0-sm.jpg', '奶粉', 'Aptamil爱他美 德国进口 幼儿配方奶粉3段 12-36月 800g\r\n德国原装进口 特有亲源配方 品牌直供', '1', '1');
+INSERT INTO `goods` VALUES (2, '帆布手提包', '1828', '京东', '34534576867345', 'https://img.alicdn.com/tfscom/TB2hhk3XHRkpuFjSspmXXc.9XXa_!!0-dgshop.jpg', 'https://img.alicdn.com/tfscom/TB2hhk3XHRkpuFjSspmXXc.9XXa_!!0-dgshop.jpg', '手提包', '复古是永恒不变的时尚亮点，不会随时间与大环境的改变而过时，来自Original Penguin的这款单肩包，使用了棕色与绿色的撞色拼接，和谐耐看，彰显淡雅平静的典雅气质。', '1', '1');
+INSERT INTO `goods` VALUES (3, '荷兰进口谷物麦片', '188', '天猫', '2436567345578', 'https://img.alicdn.com/tfscom/TB2ED.gXYBmpuFjSZFuXXaG_XXa_!!0-dgshop.jpg', 'https://img.alicdn.com/tfscom/TB2ED.gXYBmpuFjSZFuXXaG_XXa_!!0-dgshop.jpg', '谷物', ' 人食五谷身体好。除了大米以为五谷杂粮也是营养很丰富的哦。荷兰进口的混合物麦片，是早餐的不错选择哦。混合谷物麦片是魔力麦片哦，它融入了奶粉，速溶的香草乳粉麦片营养价值颇高哦', '0', '1');
+INSERT INTO `goods` VALUES (8, '创意大象毛绒公仔', '225', '唯品会', '34567456342', 'https://img.alicdn.com/tfscom/TB2pOHQXWi5V1BjSszdXXXUJXXa_!!0-dgshop.jpg', 'https://img.alicdn.com/tfscom/TB2pOHQXWi5V1BjSszdXXXUJXXa_!!0-dgshop.jpg', '家居', '听！走过来一头蓝色的大象，迈着大大步子缓缓远离尘嚣，这大概就是它喜欢做的事情吧！迈着轻快的步子外出散步，并享受着大耳朵沐浴在阳光中的舒适。', '0', '2');
+INSERT INTO `goods` VALUES (9, '麦卢卡蜂蜜酒', '290', '苏宁易购', '35365356678567', 'https://img.alicdn.com/tfscom/TB2FPkbXr0kpuFjy0FjXXcBbVXa_!!0-dgshop.jpg', 'https://img.alicdn.com/tfscom/TB2FPkbXr0kpuFjy0FjXXcBbVXa_!!0-dgshop.jpg', '酒水', '新西兰原装进口麦卢卡蜂蜜酒，采摘当地特有珍稀树种麦卢卡花蜜酿造出的蜂蜜，经过自然发酵之后成酒，口感香甜醇厚并带有花朵芳香，内含独特抗菌和抗氧化物质独麦素。', '1', '2');
 
 -- ----------------------------
 -- Table structure for orders
@@ -106,7 +112,9 @@ CREATE TABLE `shop_info`  (
 -- ----------------------------
 -- Records of shop_info
 -- ----------------------------
-INSERT INTO `shop_info` VALUES ('1', '1', '1', '1');
+INSERT INTO `shop_info` VALUES ('1', '京东超市', '苏州市吴江区金家坝镇金长路1248号', '1');
+INSERT INTO `shop_info` VALUES ('2', '天猫超市', '南京市雨花台区软件大道109号', '0');
+INSERT INTO `shop_info` VALUES ('3', '苏宁易购', '京市秦淮区中山南路49号商茂世纪广场3层', '1');
 
 -- ----------------------------
 -- Table structure for user
@@ -119,13 +127,15 @@ CREATE TABLE `user`  (
   `state` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '是否可用，0不可用，1可用',
   `type` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '0邮箱，1手机，2帐号',
   `auth_id` int(11) NOT NULL COMMENT '用户权限',
-  `shop_type` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '0普通用户 1商家',
+  `shop_type` varchar(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '0普通用户 1商家',
   PRIMARY KEY (`uuid`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '1', '1', '1', '1', 1, '1');
+INSERT INTO `user` VALUES ('1', '1', '1', '0', '1', 1, '0');
+INSERT INTO `user` VALUES ('2', '2', '2', '0', '0', 2, '1');
+INSERT INTO `user` VALUES ('3', '3', '3', '1', '2', 3, '0');
 
 SET FOREIGN_KEY_CHECKS = 1;
